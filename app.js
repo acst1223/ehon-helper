19ace7c8a0c310df2aed4fbe9a74ee8c59131202
acstAcMac <natnstart@gmail.com>
2023-01-17 16:38:18 +0900

    add multi-page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const MOUSE_UP = 0,
   MOUSE_DRAGGING = 2;
 
 let bgColor = "white";
+let color = "rgb(99, 48, 238)";
 
 let cnvs = document.getElementById("canvas");
 let ctx = cnvs.getContext("2d");
@@ -12,10 +13,93 @@ let boldSelectorList = document.querySelector(
 let boldSelectors = boldSelectorList.querySelectorAll("li");
 let clearHint = document.querySelector("div#tools div#clear-hint");
 
+let currentPage = 1;
+let maxPage = 6;
+let pageUpLabel = document.querySelector("#page-up");
+let pageDownLabel = document.querySelector("#page-down");
+let pageLabel = document.querySelector("#page");
+
 let cnvWidth = cnvs.clientWidth,
   cnvHeight = cnvs.clientHeight;
 let clickFlg = MOUSE_UP;
 let cnvBold = 1;
+
+let touchStartX, touchStartY; // the start position when touched
+
+class CnvStroke {
+  constructor(bold, color) {
+    this.bold = bold;
+    this.color = color;
+    this.points = [];
+  }
+
+  push(x, y) {
+    this.points.push([x, y]);
+  }
+}
+
+class CnvRecord {
+  constructor() {
+    this.memory = [];
+  }
+
+  // start a new edge or point
+  newEdge(x, y, bold, color) {
+    this.memory.push(new CnvStroke(bold, color));
+    this.memory[this.memory.length - 1].push(x, y);
+  }
+
+  continueEdge(x, y) {
+    this.memory[this.memory.length - 1].push(x, y);
+  }
+
+  clear() {
+    this.memory = [];
+  }
+
+  setBgColor(ctx) {
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, cnvWidth, cnvHeight);
+  }
+
+  restore(ctx) {
+    self.setBgColor(ctx);
+    for (const s of this.memory) {
+      if (s.points.length == 0) {
+        continue;
+      } else if (s.points.length == 1) {
+        let x = s.points[0][0],
+          y = s.points[0][1];
+        ctx.beginPath();
+        let r = Math.floor((s.bold + 1) / 2);
+        ctx.arc(x, y, r, 0, Math.PI * 2, true);
+        ctx.fillStyle = s.color;
+        ctx.fill();
+      } else {
+        ctx.lineWidth = s.bold;
+        ctx.strokeStyle = s.color;
+        ctx.beginPath();
+        for (let i = 0; i < s.points.length; i++) {
+          let x = s.points[i][0],
+            y = s.points[i][1];
+          if (i == 0) {
+            ctx.lineCap = "round"; //　線を角丸にする
+            ctx.moveTo(x, y);
+          } else {
+            ctx.lineTo(x, y);
+          }
+        }
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+let cnvRecords = [];
+for (let i = 0; i <= maxPage; i++) {
+  cnvRecords.push(new CnvRecord());
+}
+let cnvRecord = cnvRecords[currentPage];
 
 function initBoldSelector() {
   boldSelectors.forEach((s, i) => {
@@ -40,15 +124,17 @@ function setBgColor() {
 
 function draw(x, y) {
   ctx.lineWidth = cnvBold;
-  ctx.strokeStyle = "rgb(99, 48, 238)";
+  ctx.strokeStyle = color;
   // 初回処理の判定
   if (clickFlg == MOUSE_DOWN) {
     clickFlg = MOUSE_DRAGGING;
     ctx.beginPath();
     ctx.lineCap = "round"; //　線を角丸にする
     ctx.moveTo(x, y);
+    cnvRecord.newEdge(x, y, cnvBold, color);
   } else {
     ctx.lineTo(x, y);
+    cnvRecord.continueEdge(x, y);
   }
   ctx.stroke();
 }
@@ -57,11 +143,28 @@ for (const ev of ["mousedown", "touchstart"]) {
   cnvs.addEventListener(ev, (e) => {
     e.preventDefault();
     clickFlg = MOUSE_DOWN;
+
+    if (e.type === "touchstart") {
+      touchStartX = e.touches[0].pageX - e.touches[0].target.offsetLeft;
+      touchStartY = e.touches[0].pageY - e.touches[0].target.offsetTop;
+    }
   });
 }
 for (const ev of ["mouseup", "touchend", "touchleave", "touchcancel"]) {
   cnvs.addEventListener(ev, (e) => {
     e.preventDefault();
+    if (clickFlg == MOUSE_DOWN) {
+      if (!("offsetX" in e)) {
+        e.offsetX = touchStartX;
+        e.offsetY = touchStartY;
+      }
+      ctx.beginPath();
+      let r = Math.floor((cnvBold + 1) / 2);
+      ctx.arc(e.offsetX, e.offsetY, r, 0, Math.PI * 2, true);
+      ctx.fillStyle = color;
+      ctx.fill();
+      cnvRecord.newEdge(e.offsetX, e.offsetY, cnvBold, color);
+    }
     clickFlg = MOUSE_UP;
   });
 }
@@ -81,4 +184,34 @@ for (const ev of ["mousemove", "touchmove"]) {
 clearHint.addEventListener("click", (e) => {
   ctx.clearRect(0, 0, cnvWidth, cnvHeight);
   setBgColor();
+});
+
+function flipPage(offset) {
+  newPage = currentPage + offset;
+  if (newPage <= 1) {
+    newPage = 1;
+    pageDownLabel.classList.add("disabled");
+    pageUpLabel.classList.remove("disabled");
+  } else if (newPage >= maxPage) {
+    newPage = maxPage;
+    pageUpLabel.classList.add("disabled");
+    pageDownLabel.classList.remove("disabled");
+  } else {
+    pageUpLabel.classList.remove("disabled");
+    pageDownLabel.classList.remove("disabled");
+  }
+  pageLabel.innerHTML = String(newPage);
+  cnvRecord = cnvRecords[newPage];
+  cnvRecord.restore(ctx);
+  currentPage = newPage;
+}
+
+pageDownLabel.addEventListener("click", (e) => {
+  if (pageDownLabel.classList.contains("disabled")) return;
+  flipPage(-1);
+});
+
+pageUpLabel.addEventListener("click", (e) => {
+  if (pageUpLabel.classList.contains("disabled")) return;
+  flipPage(1);
 });
